cb63d7ccfe259b5dada974a68a2100eefdac9964
aheit <aheit@30c6eec7-5b45-4da2-9f5c-277da431b43c>
2013-12-25 21:17:25 +0000

    delete release tag in wrong folder

--- release-0.9/src/libcandbc/dbcReader.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDE_DBCREADER_H
-#define INCLUDE_DBCREADER_H
-
-#include <dbcModel.h>
-
-dbc_t *dbc_read_file(char *filename);
-
-#endif
--- release-0.9/src/libcandbc/dbcWriter.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDE_DBCWRITER_H
-#define INCLUDE_DBCWRITER_H
-
-#include <dbcModel.h>
-
-void dbc_write(FILE *out, dbc_t *dbc);
-
-#endif
