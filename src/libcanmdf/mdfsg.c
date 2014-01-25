4cb645a8925c921ad5d005e65342a89434568695
aheit <aheit@users.noreply.github.com>
2014-01-25 21:08:36 +0000

    - libcanmdf: bug fixes for big endian signals

--- src/libcanmdf/mdfsg.c
+++ src/libcanmdf/mdfsg.c
@@ -1,10 +1,12 @@
 /*  mdfsg.c --  access MDF signal groups
-    Copyright (C) 2012, 2013, 2014 Andreas Heitmann
+    Copyright (C) 2012, 2013 Andreas Heitmann
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
+
+
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,162 +22,179 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <endian.h>
 #include "mdfsg.h"
 #include "mdfmodel.h"
 
 /* convert signal to double value */
 double
 mdf_signal_convert(const uint8_t *const data_int_ptr,
-                   const mdf_t *const mdf,
-                   const cn_block_t *const cn_block)
+		   const mdf_t *const mdf,
+		   const cn_block_t *const cn_block)
 {
   /* decode */
   uint8_t bit_offset = cn_block->first_bit%8; /* LSB */
   cc_block_t *cc_block = cc_block_get(mdf,
-                                      cn_block->link_conversion_formula);
+				      cn_block->link_conversion_formula);
   double converted_double;
   int64_t data_int64;
   double data_ieee754;
   uint16_t number_bits = cn_block->number_bits;
+  uint16_t default_byte_order_big_endian = id_block_get(mdf)->byte_order;
+  const sdt = cn_block->signal_data_type;
+  const int cn_is_big_endian =
+       (sdt == sdt_unsigned_int_big_endian)
+    || (sdt == sdt_signed_int_big_endian)
+    || (sdt == sdt_ieee754_float_big_endian)
+    || (sdt == sdt_ieee754_double_big_endian)
+    || (default_byte_order_big_endian &&
+	(   (sdt == sdt_unsigned_int_default)
+         || (sdt == sdt_signed_int_default)
+         || (sdt == sdt_ieee754_float_default)
+         || (sdt == sdt_ieee754_double_default)));
+  /* swap words if channel endianess differs from machine endianess */
+#ifdef WORDS_BIGENDIAN
+  const int swap = !cn_is_big_endian;
+#else
+  const int swap = cn_is_big_endian;
+#endif
 
   /* extract data */
-  switch(cn_block->signal_data_type) {
-  case 0: /* unsigned integer (Intel) */
-  case 9: /* unsigned integer (Motorola) */
+  switch(sdt) {
+  case sdt_unsigned_int_default:
+  case sdt_unsigned_int_big_endian:
+  case sdt_unsigned_int_little_endian:
     if(number_bits == 1) {
       data_int64 = ((*(uint8_t *)data_int_ptr) >> bit_offset) & 1;
     } else if(number_bits <= 9) {
       data_int64 = ((*(uint16_t *)data_int_ptr) >> bit_offset)
-        & ((1u<<number_bits)-1u);
-      if(cn_block->signal_data_type == 9) {
-        uint8_t *dp = (uint8_t *)&data_int64;
-        uint8_t d;
-        d     = dp[0];
-        dp[0] = dp[1];
-        dp[1] = d;
+	& ((1u<<number_bits)-1u);
+      if(number_bits>8) {
+	if(swap) {
+	  /*
+	  printf("UI %04x (%d,%d,%d,%d,%d)\t",*(uint16_t *)data_int_ptr,
+		 number_bits, bit_offset, sdt, cn_is_big_endian, swap);
+	  */
+	  data_int64 = bswap_16(*(uint16_t *)&data_int64);
+	  /*
+	  printf("-> %016llx\n", data_int64);
+	  */
+	}
       }
     } else if(number_bits <= 25) {
       data_int64 = ((*(uint32_t *)data_int_ptr) >> bit_offset)
-        & ((1u<<number_bits)-1u);
-      if(cn_block->signal_data_type == 9) {
-        uint8_t *dp = (uint8_t *)&data_int64;
-        uint8_t d;
-        d     = dp[0];
-        dp[0] = dp[3];
-        dp[3] = d;
-        d     = dp[1];
-        dp[1] = dp[2];
-        dp[2] = d;
+	& ((1u<<number_bits)-1u);
+      if(swap) {
+	/*
+	printf("UI %08x (%d,%d,%d,%d,%d)\t",*(uint32_t *)data_int_ptr,
+	       number_bits, bit_offset, sdt, cn_is_big_endian, swap);
+	*/
+	if(number_bits > 16) {
+	  data_int64 = bswap_32(*(uint32_t *)&data_int64);
+	} else {
+	  data_int64 = bswap_16(*(uint16_t *)&data_int64);
+	}
+	/*
+	printf("-> %016llx\n", data_int64);
+	*/
       }
     } else {
       assert(bit_offset + number_bits <= 64);
       data_int64 = ((*(uint64_t *)data_int_ptr) >> bit_offset)
-        & (((uint64_t)1<<number_bits)-(uint64_t)1);
-      if(cn_block->signal_data_type == 9) {
-        uint8_t *dp = (uint8_t *)&data_int64;
-        uint8_t d;
-        d     = dp[0];
-        dp[0] = dp[7];
-        dp[7] = d;
-        d     = dp[1];
-        dp[1] = dp[6];
-        dp[6] = d;
-        d     = dp[2];
-        dp[2] = dp[5];
-        dp[5] = d;
-        d     = dp[3];
-        dp[3] = dp[4];
-        dp[4] = d;
-      }
+	& (((uint64_t)1<<number_bits)-(uint64_t)1);
+      if(swap) {
+	/*
+	printf("UI %16llx (%d,%d,%d,%d,%d)\t",*(uint64_t *)data_int_ptr,
+	       number_bits, bit_offset, sdt, cn_is_big_endian, swap);
+	*/
+	if(number_bits>32) {
+	  data_int64 = bswap_64(*(uint64_t *)&data_int64);
+	} else {
+	  data_int64 = bswap_32(*(uint32_t *)&data_int64);
+	}
+	/*
+	printf("-> %016llx\n", data_int64);
+	*/
+      } 
     }
     break;
-  case 1: /* signed integer (Intel) */
-  case 10: /* signed integer (Motorola) */
+  case sdt_signed_int_default:
+  case sdt_signed_int_big_endian:
+  case sdt_signed_int_little_endian:
     if(number_bits <= 9) {
-      if(cn_block->signal_data_type == 1) {
-        data_int64  =
-          (int64_t)(((int16_t)
-                     ((*(uint16_t *)data_int_ptr)
-                      << (16-number_bits-bit_offset)))
-                    >>(16-number_bits));
-        /* printf("%4x %d %d -> %16llx\n",
-         *(uint16_t *)data_int_ptr, number_bits, bit_offset,
-         data_int64); */
-      } else {
-        uint16_t data_u16 = ((*(uint16_t *)data_int_ptr) 
-                             << (16-number_bits-bit_offset));
-        uint8_t *dp = (uint8_t *)&data_u16;
-        uint8_t d;
-        d     = dp[0];
-        dp[0] = dp[1];
-        dp[1] = d;
-        data_int64 = (int64_t)(((int16_t)data_u16) >> (16-number_bits));
+      data_int64  =
+	  (int64_t)(((int16_t)
+		     ((*(uint16_t *)data_int_ptr)
+		      << (16-number_bits-bit_offset)))
+		    >>(16-number_bits));
+      if(number_bits>8) {
+	if(swap) {
+	  data_int64 = bswap_16(*(uint16_t *)&data_int64);
+	} else {
+	}
       }
     } else if(number_bits <= 25) {
-      if(cn_block->signal_data_type == 1) {
-        data_int64  =
-          (int64_t)(((int32_t)
-                     ((*(uint32_t *)data_int_ptr)
-                      << (32-number_bits-bit_offset)))
-                    >>(32-number_bits));
-        /* printf("%8x %d %d -> %16llx\n",*(uint32_t *)data_int_ptr,
-           number_bits, bit_offset, data_int64);  */
-      } else {
-        uint32_t data_u32 = ((*(uint32_t *)data_int_ptr)
-                             << (32-number_bits-bit_offset));
-        uint8_t *dp = (uint8_t *)&data_u32;
-        uint8_t d;
-        d     = dp[0];
-        dp[0] = dp[3];
-        dp[3] = d;
-        d     = dp[1];
-        dp[1] = dp[2];
-        dp[2] = d;
-        data_int64 = (int64_t)(((int32_t)data_u32) >> (32-number_bits));
+      uint32_t data_u32;
+
+      data_u32 = ((*(uint32_t *)data_int_ptr) << (32-number_bits-bit_offset));
+      if(swap) {
+	if(number_bits > 16) {
+	  data_u32 = bswap_32(*(uint32_t *)&data_u32);
+	} else {
+	  data_u32 = bswap_16(*(uint16_t *)&data_u32);
+	}
       }
+      data_int64 = (int64_t)(((int32_t)data_u32) >> (32-number_bits));
+      /*
+	printf("SI16 %08lx %d %d %d %d -> %016llx\n",*(uint32_t *)data_int_ptr,
+	       number_bits, bit_offset, cn_is_big_endian, swap, data_int64);
+      */
     } else {
+      uint64_t data_u64;
+
       assert(bit_offset + number_bits <= 64);
-      if(cn_block->signal_data_type == 1) {
-        data_int64  =
-          (((int64_t)
-            ((*(uint64_t *)data_int_ptr)
-             << (64-number_bits-bit_offset)))
-           >>(64-number_bits));
-        /* printf("%16llx %d %d -> %16llx\n",*(uint64_t *)data_int_ptr,
-           number_bits, bit_offset, data_int64); */
-      } else {
-        uint64_t data_u64 = ((*(uint64_t *)data_int_ptr)
-                             << (64-number_bits-bit_offset));
-        uint8_t *dp = (uint8_t *)&data_u64;
-        uint8_t d;
-        d     = dp[0];
-        dp[0] = dp[7];
-        dp[7] = d;
-        d     = dp[1];
-        dp[1] = dp[6];
-        dp[6] = d;
-        d     = dp[2];
-        dp[2] = dp[5];
-        dp[5] = d;
-        d     = dp[3];
-        dp[3] = dp[4];
-        dp[4] = d;
-        data_int64 = (int64_t)(((int64_t)data_u64) >> (64-number_bits));
+      data_u64 = ((*(uint64_t *)data_int_ptr)
+		  << (64-number_bits-bit_offset));
+      if(swap) {
+	data_u64 = bswap_64(*(uint64_t *)&data_u64);
       }
+      data_int64 = (int64_t)(((int64_t)data_u64) >> (64-number_bits));
     }
     break;
-  case 2: /* float */
-    data_ieee754 = *(float *)data_int_ptr;
+  case sdt_ieee754_float_default:
+  case sdt_ieee754_float_big_endian:
+  case sdt_ieee754_float_little_endian:
+    {
+      uint32_t data_u32 = *(uint32_t *)data_int_ptr;
+      if(swap) {
+	 data_u32 = bswap_32(*(uint32_t *)&data_u32);
+      }
+      data_ieee754 = *(float *)&data_u32;
+      /*
+	printf("FLOAT (%d) 0x%x %d 0x%x %g\n", sdt, *(uint32_t *)data_int_ptr, swap, data_u32,  data_ieee754);
+      */
+    }
     break;
-  case 3: /* double */
-    data_ieee754 = *(double *)data_int_ptr;
+  case sdt_ieee754_double_default:
+  case sdt_ieee754_double_big_endian:
+  case sdt_ieee754_double_little_endian:
+    {
+      uint64_t data_u64 = *(uint64_t *)data_int_ptr;
+      if(swap) {
+	 data_u64 = bswap_64(*(uint64_t *)&data_u64);
+      }
+      data_ieee754 = *(double *)&data_u64;
+      /* 
+      printf("DOUBLE (%d) 0x%llx %d 0x%llx %lg\n", sdt, *(uint64_t *)data_int_ptr, swap, data_u64,  data_ieee754);
+      */
+    }
     break;
-  case 7: /* string */
+  case sdt_string: /* string */
     data_int64 = 0;
     break;
   default:
     fprintf(stderr,"signal_data_type %hu not implemented\n",
-            (unsigned short)cn_block->signal_data_type);
+	    (unsigned short)sdt);
     exit(EXIT_FAILURE);
   }
 
@@ -183,13 +202,27 @@ mdf_signal_convert(const uint8_t *const data_int_ptr,
   if(cc_block) {
     switch(cc_block->conversion_type) {
     case 0:
-      if(cn_block->signal_data_type == 3) {
-        converted_double = data_ieee754 * cc_block->supplement.linear.p2
-          +cc_block->supplement.linear.p1;
-        /* printf("%lg -> %lg\n",data_ieee754,converted_double); */
-      } else {
-        converted_double = data_int64 * cc_block->supplement.linear.p2
-          +cc_block->supplement.linear.p1;
+      switch(sdt) {
+      case sdt_unsigned_int_default:
+      case sdt_signed_int_default:
+      case sdt_string:
+      case sdt_unsigned_int_big_endian:
+      case sdt_signed_int_big_endian:
+	converted_double = data_int64 * cc_block->supplement.linear.p2
+	  +cc_block->supplement.linear.p1;
+	/* printf("%ld -> %lg\n",data_int64,converted_double); */
+	break;
+      case sdt_ieee754_float_default:
+      case sdt_ieee754_float_big_endian:
+      case sdt_ieee754_float_little_endian:
+      case sdt_ieee754_double_default:
+      case sdt_ieee754_double_big_endian:
+      case sdt_ieee754_double_little_endian:
+	converted_double = data_ieee754 * cc_block->supplement.linear.p2
+	                 + cc_block->supplement.linear.p1;
+	break;
+      default:
+	assert(1);
       }
       break;
     case 12:
@@ -197,23 +230,28 @@ mdf_signal_convert(const uint8_t *const data_int_ptr,
       break;
     default:
       fprintf(stderr,"conversion %hu not implemented\n",
-              (unsigned short)cc_block->conversion_type);
+	      (unsigned short)cc_block->conversion_type);
       exit(EXIT_FAILURE);
     }
   } else {
-    switch(cn_block->signal_data_type) {
-    case 0:
-    case 1:
-    case 7:
+    switch(sdt) {
+      case sdt_unsigned_int_default:
+      case sdt_signed_int_default:
+      case sdt_string:
+      case sdt_unsigned_int_big_endian:
+      case sdt_signed_int_big_endian:
       converted_double = data_int64;
       break;
-    case 2:
-    case 3:
+    case sdt_ieee754_float_default:
+    case sdt_ieee754_double_default:
+    case sdt_ieee754_float_big_endian:
       converted_double = data_ieee754;
+      break;
     default:
       assert(1);
     }
   }
+  /* printf("CD %lg\n",converted_double); */
   return converted_double;
 }
 
