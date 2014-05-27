969a12993ef0dc5362c379ec764d8b7365db4d89
aheit <aheit@users.noreply.github.com>
2014-05-27 19:25:05 +0000

    * added directory cantomat

---
+++ src/cantomat/messageDecoder.h
@@ -0,0 +1,20 @@
+#ifndef INCLUDE_MESSAGEDECODER_H
+#define INCLUDE_MESSAGEDECODER_H
+
+#include "dbcModel.h"
+#include "measurement.h"
+
+/* signal procesing callback function */
+typedef void (* signalProcCb_t)(const signal_t     *s,
+				double              dtime,
+				uint32              rawValue,
+				double              physicalValue,
+				void               *cbData);
+
+void canMessage_decode(message_t      *dbcMessage,
+		       canMessage_t   *canMessage,
+		       sint32          timeResolution,
+		       signalProcCb_t  signalProcCb,
+		       void           *cbData);
+
+#endif
